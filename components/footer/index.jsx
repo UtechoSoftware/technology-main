14cadbb15ad698f0fb169d2a8b5285725346def7
Danish Goheer <danishgoheer17@gmail.com>
2025-08-04 04:08:02 +0500

    changes

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -189,7 +189,7 @@ const Footer = () => {
                       Phone
                     </h4>
                     <div className="text-gray-500 dark:text-gray-400 text-base space-y-1">
-                      <a href="tel:+2018918201">+201-891-8201</a>
+                      <a href="tel:888-425-573-2873">888-425-573-2873</a>
                     </div>
                   </div>
                 </div>
